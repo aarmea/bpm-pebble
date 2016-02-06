6abc42bab4e7ca8abfc6ed362dbb9125d9869344
Albert Armea <albert@albertarmea.com>
2016-02-05 23:54:19 -0800

    Add play/pause icon resources

--- src/icons.c
+++ src/icons.c
@@ -1,9 +1,13 @@
 #include "icons.h"
 
 GBitmap *ICON_TEMPO_TAP;
+GBitmap *ICON_PLAY;
+GBitmap *ICON_PAUSE;
 
 void icons_init() {
   ICON_TEMPO_TAP = gbitmap_create_with_resource(RESOURCE_ID_ICON_TEMPO_TAP);
+  ICON_PLAY = gbitmap_create_with_resource(RESOURCE_ID_ICON_PLAY);
+  ICON_PAUSE = gbitmap_create_with_resource(RESOURCE_ID_ICON_PAUSE);
 }
 
 void icons_deinit() {
--- src/icons.h
+++ src/icons.h
@@ -1,6 +1,8 @@
 #include "pebble.h"
 
 extern GBitmap *ICON_TEMPO_TAP;
+extern GBitmap *ICON_PLAY;
+extern GBitmap *ICON_PAUSE;
 
 void icons_init();
 void icons_deinit();
